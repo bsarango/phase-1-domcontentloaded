c3c8eadc7f7d5d822a10548435cff0af4e507879
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-05-09 15:48:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+function replaceText(){    //e is the event that happens; in this case 'DOMContentLoaded'
+    const p = document.querySelector('#text')   //p with ID 'text'
+    p.textContent = "This is really cool!"
+    console.log(p.textContent)
+}
+
+document.addEventListener('DOMContentLoaded',replaceText)
+
